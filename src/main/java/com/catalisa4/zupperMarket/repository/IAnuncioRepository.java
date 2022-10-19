2af0bc98381fe7343c7dc9451d56e867e2706a83
davispacheco <davipacheco1996@gmail.com>
2022-10-19 18:47:25 -0300

    Diretório repository: renomear interface de AnuncioRepository para IAnuncioRepository.

--- src/main/java/com/catalisa4/zupperMarket/repository/IAnuncioRepository.java
+++ src/main/java/com/catalisa4/zupperMarket/repository/IAnuncioRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AnuncioRepository extends JpaRepository<AnunciosModel, Long> {
+public interface IAnuncioRepository extends JpaRepository<AnunciosModel, Long> {
 }
