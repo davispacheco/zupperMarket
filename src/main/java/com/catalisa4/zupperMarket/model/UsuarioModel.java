648cf995da908287ee31a0dc8036d0724581d935
davispacheco <davipacheco1996@gmail.com>
2022-10-19 12:03:14 -0300

    Criar classe UsuarioModel, para modelagem da entidade usuarios no banco de dados.

---
+++ src/main/java/com/catalisa4/zupperMarket/model/UsuarioModel.java
@@ -0,0 +1,29 @@
+package com.catalisa4.zupperMarket.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "usuarios")
+public class UsuarioModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nomeCompleto;
+
+    @Column(nullable = false)
+    private String apelido;
+
+    @Column(nullable = false)
+    private String email;
+
+    private String celular;
+}
