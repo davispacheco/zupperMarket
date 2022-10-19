eff82b5794c7eafde224c8b98d2ff25c695ee2da
cast10 <castanha.douglas2013@gmail.com>
2022-10-19 14:24:59 -0300

    Criar Enun Categoria Criar Anuncios Model

---
+++ src/main/java/com/catalisa4/zupperMarket/Enuns/Categorias.java
@@ -0,0 +1,25 @@
+package com.catalisa4.zupperMarket.Enuns;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum Categorias {
+
+    SERVICOS,
+
+    INGRESSOS,
+
+    MOVEIS,
+
+    VEICULOS,
+
+    TECNOLOGIA,
+
+    GAMES,
+
+    ELETRODOMESTICOS,
+
+    ESPORTE,
+
+    OUTROS
+}
