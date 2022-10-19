8ab86c0568c5442e45907e45050ab701a8016345
davispacheco <davipacheco1996@gmail.com>
2022-10-19 12:08:30 -0300

    Classe UsuarioModel: colocar o atributo/coluna email como sendo único.

--- src/main/java/com/catalisa4/zupperMarket/model/UsuarioModel.java
+++ src/main/java/com/catalisa4/zupperMarket/model/UsuarioModel.java
@@ -22,7 +22,7 @@ public class UsuarioModel {
     @Column(nullable = false)
     private String apelido;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String email;
 
     private String celular;
