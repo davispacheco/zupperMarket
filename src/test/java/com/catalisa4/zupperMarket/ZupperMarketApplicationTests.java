b3c4f99355aacdd80eef8213654c95f98ae86a98
davispacheco <davipacheco1996@gmail.com>
2022-10-18 21:23:34 -0300

    Subir parte inicial do projeto.

---
+++ src/test/java/com/catalisa4/zupperMarket/ZupperMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catalisa4.zupperMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZupperMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
