73e0f57af5b994423a7a9984ed25ddc76e49d95c
cast10 <castanha.douglas2013@gmail.com>
2022-10-19 14:07:43 -0300

    Criar Enun FormasDeEntrega

---
+++ src/main/java/com/catalisa4/zupperMarket/Enuns/FormasDeEntrega.java
@@ -0,0 +1,13 @@
+package com.catalisa4.zupperMarket.Enuns;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum FormasDeEntrega {
+
+    TRANSPORTADORA,
+
+    RETIRADANOLOCAL,
+
+    ENVIOPOREMAIL
+}
