494995ddd7eff3a0696ec32eb9ad85a35f1038d5
davispacheco <davipacheco1996@gmail.com>
2022-10-18 21:23:34 -0300

    Subir parte inicial do projeto.

---
+++ src/main/java/com/catalisa4/zupperMarket/ZupperMarketApplication.java
@@ -0,0 +1,13 @@
+package com.catalisa4.zupperMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZupperMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZupperMarketApplication.class, args);
+	}
+
+}
