d0ad5b0a399a5f1f9effd95f4cb5be24d204a50a
davispacheco <davipacheco1996@gmail.com>
2022-10-19 15:50:18 -0300

    Classe UsuarioController: adicionar injeção de dependências para indicar a utilização dos métodos da classe UsuarioService.

--- src/main/java/com/catalisa4/zupperMarket/controller/UsuarioController.java
+++ src/main/java/com/catalisa4/zupperMarket/controller/UsuarioController.java
@@ -2,12 +2,17 @@ package com.catalisa4.zupperMarket.controller;
 
 import com.catalisa4.zupperMarket.DTO.UsuarioRequest;
 import com.catalisa4.zupperMarket.DTO.UsuarioResponse;
+import com.catalisa4.zupperMarket.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/usuarios")
 public class UsuarioController {
+    @Autowired
+    private UsuarioService usuarioService;
+
     @CrossOrigin(origins = "http://127.0.0.1:5500")
     @PostMapping
     public ResponseEntity<UsuarioResponse> cadastrarUsuario(@RequestBody UsuarioRequest usuarioRequest) {
