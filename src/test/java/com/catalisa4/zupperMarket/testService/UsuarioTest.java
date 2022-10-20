76b4df0bbfb308240bcd607c4fcdb9d9f20e54f9
analistakatia <katiadocita@gmail.com>
2022-10-20 10:58:55 -0300

    Implementando testes Service

--- src/test/java/com/catalisa4/zupperMarket/testService/UsuarioTest.java
+++ src/test/java/com/catalisa4/zupperMarket/testService/UsuarioTest.java
@@ -3,17 +3,21 @@ package com.catalisa4.zupperMarket.testService;
 import com.catalisa4.zupperMarket.model.UsuarioModel;
 import com.catalisa4.zupperMarket.repository.IUsuarioRepository;
 import com.catalisa4.zupperMarket.service.UsuarioService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -21,7 +25,12 @@ import static org.mockito.Mockito.when;
 @SpringBootTest
 public class UsuarioTest {
 
-    @Mock
+    public static final String NOME_COMPLETO = "Katia Queiroz";
+    public static final String APELIDO = "katinha";
+    public static final String EMAIL = "katia@zup.com.br";
+    public static final String CELULAR = "3499999999";
+    public static final String SENHA = "1234";
+    @InjectMocks
     UsuarioService usuarioService;
 
     @MockBean
@@ -29,10 +38,12 @@ public class UsuarioTest {
 
     private UsuarioModel usuarioModel;
 
+    private Optional<UsuarioModel> optionalUsuarioModel;
+
     @BeforeEach
-    private void inicializador() {
+    private void setUsuarioService() {
         MockitoAnnotations.openMocks(this);
-        usuarioModel = new UsuarioModel();
+        startUsuario();
     }
 
     @Test
@@ -42,5 +53,20 @@ public class UsuarioTest {
         when(iUsuarioRepository.existsBYEmail(Mockito.anyString())).thenReturn(true);
         Mockito.verify(iUsuarioRepository, Mockito.times(1)).save(usuarioModel);
     }
+
+    @Test
+    @DisplayName("O teste deve retornar true, se o metodo de buscar por id estiver funcionando corretamente")
+    void testeDeBuscarUsuarioViaId() {
+        usuarioService.exibirUsuarioPorId(1L);
+        Assertions.assertTrue(true);
+    }
+
+    private void startUsuario(){
+        usuarioModel = new UsuarioModel(NOME_COMPLETO, APELIDO, EMAIL, CELULAR, SENHA);
+        optionalUsuarioModel = Optional.of(new UsuarioModel(NOME_COMPLETO, APELIDO, EMAIL, CELULAR, SENHA));
+
+    }
+
+
 
 }
