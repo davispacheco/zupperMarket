e07b5e05eeda4d2f8de863e84dd840691395f9d7
davispacheco <davipacheco1996@gmail.com>
2022-10-25 18:06:01 -0300

    Classe AnuncioService: adicionar chamada aos métodos findByStatus e findByStatusAndCategoria, criando os métodos buscarPorStatus e buscarPorStatusECategoria.

--- src/main/java/com/catalisa4/zupperMarket/service/AnuncioService.java
+++ src/main/java/com/catalisa4/zupperMarket/service/AnuncioService.java
@@ -1,6 +1,7 @@
 package com.catalisa4.zupperMarket.service;
 
 
+import com.catalisa4.zupperMarket.enums.Categoria;
 import com.catalisa4.zupperMarket.enums.Status;
 import com.catalisa4.zupperMarket.exception.EntityNotFoundException;
 import com.catalisa4.zupperMarket.model.AnuncioModel;
@@ -37,6 +38,13 @@ public class AnuncioService {
         return obj.get();
     }
 
+    public List<AnuncioModel> buscarPorStatus(Status status) {
+        return iAnuncioRepository.findByStatus(status);
+    }
+
+    public List<AnuncioModel> buscarPorStatusECategoria(Status status, Categoria categoria) {
+        return iAnuncioRepository.findByStatusAndCategoria(status, categoria);
+    }
     public AnuncioModel cadastrarNovoAnuncio(AnuncioModel anuncioModel, Long id) {
         anuncioModel.setStatus(Status.DISPONIVEL);
         anuncioModel.setDataHoraCriacao(LocalDateTime.now());
@@ -44,7 +52,7 @@ public class AnuncioService {
 
         usuario.orElseThrow((() -> new EntityNotFoundException("Usuário com o id " + id + " não encontrado.")));
 
-        
+
         anuncioModel.setUsuario(usuario.get());
         return iAnuncioRepository.save(anuncioModel);
     }
