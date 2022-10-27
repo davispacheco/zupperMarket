f3a4b9536fca1ffdbc6acedba2bac3c61d1aee99
davispacheco <davipacheco1996@gmail.com>
2022-10-27 15:56:14 -0300

    Classe AnuncioController: adicionar anotação @CrossOrigin a cada requisição da aplicação.

--- src/main/java/com/catalisa4/zupperMarket/controller/AnuncioController.java
+++ src/main/java/com/catalisa4/zupperMarket/controller/AnuncioController.java
@@ -21,29 +21,34 @@ public class AnuncioController {
     @Autowired
     private AnuncioService anuncioService;
 
+    @CrossOrigin(origins = "http://127.0.0.1:5500")
     @GetMapping
     public ResponseEntity<List<AnuncioResponse>> buscarAnuncios (){
         List<AnuncioModel> listaDeAnunciosModel = anuncioService.buscarTodosAnuncios();
         return ResponseEntity.ok(AnuncioResponse.fromAnuncioModelList(listaDeAnunciosModel));
     }
 
+    @CrossOrigin(origins = "http://127.0.0.1:5500")
     @GetMapping(path = "/status")
     public ResponseEntity<List<AnuncioResponse>> buscarAnunciosPorStatus(@RequestParam Status status) {
         List<AnuncioModel> anuncios = anuncioService.buscarPorStatus(status);
         return ResponseEntity.ok(AnuncioResponse.fromAnuncioModelList(anuncios));
     }
 
+    @CrossOrigin(origins = "http://127.0.0.1:5500")
     @GetMapping(path = "/categoria")
     public ResponseEntity<List<AnuncioResponse>> buscarAnunciosPorStatusECategorias(@RequestParam Status status, @RequestParam Categoria categoria){
         List<AnuncioModel> anuncios = anuncioService.buscarPorStatusECategoria(status, categoria);
         return ResponseEntity.ok(AnuncioResponse.fromAnuncioModelList(anuncios));
     }
 
+    @CrossOrigin(origins = "http://127.0.0.1:5500")
     @GetMapping (path = "/{id}")
     public ResponseEntity<AnuncioModel> buscarAnuncioPorId(@PathVariable Long id){
         return ResponseEntity.ok(anuncioService.buscarPorId(id));
     }
 
+    @CrossOrigin(origins = "http://127.0.0.1:5500")
     @PostMapping
     public ResponseEntity<AnuncioResponse> cadastrarAnuncio(@Valid @RequestBody AnuncioRequest anuncioRequest){
         AnuncioModel anuncio = anuncioService.cadastrarNovoAnuncio(anuncioRequest.toAnuncioModel(), anuncioRequest.getUsuarioId());
@@ -51,6 +56,7 @@ public class AnuncioController {
         return new ResponseEntity<>(anuncioResponse, HttpStatus.CREATED);
     }
 
+    @CrossOrigin(origins = "http://127.0.0.1:5500")
     @PutMapping (path = "/{id}")
     public ResponseEntity<AnuncioResponse> alterarAnuncio(@RequestBody AnuncioRequest anuncioRequest, @PathVariable Long id) {
         AnuncioModel anuncioAlterado = anuncioService.alterarAnuncio(anuncioRequest.toAnuncioModel());
@@ -58,6 +64,7 @@ public class AnuncioController {
         return  ResponseEntity.ok(anuncioResponse); //verificar se está correto
     }
 
+    @CrossOrigin(origins = "http://127.0.0.1:5500")
     @PatchMapping(path = "/{id}")
     public ResponseEntity<AnuncioResponse> alterarStatusDoAnuncio(@RequestBody AnuncioRequestStatusOnly anuncioRequestStatusOnly, @PathVariable Long id) {
         AnuncioModel anuncioAlterado = anuncioService.alterarStatusAnuncio(anuncioRequestStatusOnly.toAnuncioModel());
@@ -65,6 +72,7 @@ public class AnuncioController {
         return  ResponseEntity.ok(anuncioResponse); //verificar se está correto
     }
 
+    @CrossOrigin(origins = "http://127.0.0.1:5500")
     @DeleteMapping(path = "/{id}")
     public ResponseEntity<?> deletarAnuncio(@PathVariable Long id) {
         anuncioService.deletarAnuncio(id);
